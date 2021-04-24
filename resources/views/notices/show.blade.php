8ff3a801daa5e46b80a3d8e262c01d657205aaf8
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-04-25 04:47:29 +0600

    APWT Assignment

---
+++ resources/views/notices/show.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+
+<h1>{{ $notice->title }}</h1>
+<small>{{ $notice->created_at }}</small>
+<p>{{ $notice->desc }}</p>
+
+@if(Auth::check())
+<a href="http://localhost/atp/public/notice/{{ $notice->id }}/edit" class="btn btn-primary">Edit</a>
+
+{!! Form::open(['url' => ['notice', $notice->id], 'method' => 'POST'])  !!}
+{{ Form::hidden('_method', 'DELETE') }}
+{{ Form::submit('Delete', ['class' => 'btn btn-danger']) }}
+{!! Form::close() !!}
+@endif
+@endsection
